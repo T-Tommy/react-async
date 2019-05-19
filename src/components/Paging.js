4a793917041d16675e69cf82049bbdf1bf223efe
Tommy Tran <tranttommy95@gmail.com>
2019-05-19 00:38:37 -0700

    refactored Paging

--- src/components/Paging.js
+++ src/components/Paging.js
@@ -1,19 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-function Paging({ prevPage, nextPage, page, totalPages }) {
+function Paging({ updatePage, page, totalPages }) {
   return (
     <nav>
-      <button onClick={ prevPage } disabled={ 1 === page }>{ '<<' }</button>
+      <button onClick={ () => updatePage(page - 1) } disabled={ 1 === page }>{ '<<' }</button>
       Page { page } of { totalPages }
-      <button onClick={ nextPage } disabled={ totalPages === page }>{ '>>' }</button>
+      <button onClick={ () => updatePage(page + 1) } disabled={ totalPages === page }>{ '>>' }</button>
     </nav>
   );
 }
 
 Paging.propTypes = {
-  prevPage: PropTypes.func.isRequired,
-  nextPage: PropTypes.func.isRequired,
+  updatePage: PropTypes.func.isRequired,
   page: PropTypes.number.isRequired,
   totalPages: PropTypes.number.isRequired
 };
