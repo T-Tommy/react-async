a097f7fd623c7bea87db1a72a623540a3144fb6d
Tommy Tran <tranttommy95@gmail.com>
2019-05-22 10:48:48 -0700

    added episode component

--- src/services/selectFields.js
+++ src/services/selectFields.js
@@ -1,7 +1,9 @@
 import Character from '../components/Character';
 import Location from '../components/Location';
+import Episode from '../components/Episode';
 
 export default [
   Character,
-  Location
+  Location,
+  Episode
 ];
